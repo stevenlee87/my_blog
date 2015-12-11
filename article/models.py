cb7b21d42b2c5673fe02ac4fe80e0a37e7b551a7
stevenlee87 <imepisode1@gmail.com>
2015-12-11 19:29:09 +0800

    add duoshuo and markdown

--- article/models.py
+++ article/models.py
@@ -1,4 +1,6 @@
+# coding:utf-8
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -6,6 +8,11 @@ class Article(models.Model):
     category = models.CharField(max_length = 50, blank=True) #blog category
     date_time = models.DateTimeField(auto_now_add = True) #blog datetime
     content = models.TextField(blank = True, null = True) #blog content
+
+    #获取URL并转换成url的表示格式
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://192.168.147.133:8000%s" % path
 
     def __unicode__(self):
         return self.title
