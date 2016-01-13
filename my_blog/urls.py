f8499623d7ceaa4c6e0addd02e25c8e2d677ed84
stevenlee87 <imepisode1@gmail.com>
2016-01-13 18:09:22 +0800

    add archive and aboutme

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -2,9 +2,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-#千万不能写成from my_blog.article import views as articleviews,这样会找不到article app
-from article import views as articleviews
-from article.templatetags import custom_markdown 
+#from article import views as articleviews
+#from article.templatetags import custom_markdown 
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,9 +12,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', articleviews.home),
-    #url(r'^$', article.views.home),
+    #url(r'^$', articleviews.home),
+    url(r'^$', 'article.views.home', name = 'home'),
     #url(r'^(?P<my_args>\d+)/$', articleviews.detail, name='detail'),
     #url(r'^test/$', articleviews.test),
     url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^archives/$', 'article.views.archives', name = 'archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    #url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+    #url(r'^search/$','article.views.blog_search', name = 'search'),
 )
