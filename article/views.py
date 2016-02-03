4e9b79d300f483e5f7de8318f462932261c9459f
stevenlee87 <imepisode1@gmail.com>
2016-02-03 11:30:39 +0800

    Add search and readmore function

--- article/views.py
+++ article/views.py
@@ -39,6 +39,7 @@ def archives(request):
         raise Http404
     return render(request, 'archives.html', {'post_list' : post_list, 
                                             'error' : False})
+
 def about_me(request):
     return render(request, 'aboutme.html')
 
@@ -48,3 +49,18 @@ def search_tag(request, tag):
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'tag.html', {'post_list' : post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
